efa6b8f0ec610d20c9a3083865536e56c08e06cc
Drxpz08 <rodrigomiguelbgomes@gmail.com>
2025-09-17 12:13:17 +0100

    Frist commit. Criar classe Aluno

---
+++ Aluno/Aluno.java
@@ -0,0 +1,95 @@
+
+/**
+ * Escreva uma descrição da classe Aluno aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Aluno
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int id;
+    private int numProcesso;
+    private String nome;
+    private String dataNascimento;
+    private String morada;
+    private int telefone;
+
+
+    /**
+     * Construtor para objetos da classe Aluno
+     */
+    public Aluno()
+    {
+        // inicializa variáveis de instância
+        
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int getId()
+    {
+        return this.id;
+    }
+    
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    
+    public int getNumProcesso()
+    {
+        return this.numProcesso;
+    }
+    
+    public void setNumProcesso(int numProcesso)
+    {
+        this.numProcesso = numProcesso;
+    }
+    
+    public String getNome()
+    {
+        return this.nome;
+    }
+    
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+    
+    public String getDataNascimento()
+    {
+        return this.dataNascimento;
+    }
+    
+    public void setDataNascimento(String dataNascimento)
+    {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    public String getmorada()
+    {
+        return this.morada;
+    }
+    
+    public void setMorada(String morada)
+    {
+        this.morada = morada;
+    }
+    
+    public int getTelefone()
+    {
+        return this.telefone;
+    }
+    
+    public void setTelefone(int telefone)
+    {
+        this.telefone = telefone;
+    }
+
+
+}
