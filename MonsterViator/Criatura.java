bce856b90bc31d4120e823782f7cebadb872f6dc
Rodrigo Miguel Baptista Gomes <37610@esjaloures.org>
2025-10-30 11:12:12 +0000

    Tudo commitado

---
+++ MonsterViator/Criatura.java
@@ -0,0 +1,25 @@
+
+/**
+ * Escreva uma descrição da classe Criatura aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Criatura
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String nome;
+    private String reino;
+    
+    public Criatura(String nome)
+    {
+        // inicializa variáveis de instância
+        this.nome = nome;
+    }
+
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return 1;
+    }
+}
